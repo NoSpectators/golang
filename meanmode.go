df41e904ecd0f4c5162a4860c638b94325655200
John Erickson <john.c.erickson@gmail.com>
2018-08-16 12:46:51 -0400

    added meanmode coderbyte sol

---
+++ meanmode.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+)
+
+func MeanMode(arr []int) int { 
+	//create dictionary/hash 
+	//computer mean 
+	arrMap := make(map[int]int)
+	total := 0
+	length := len(arr)
+	for _, val := range arr {
+		total += val
+		arrMap[val]++
+	}
+	mean := float64(total/length)
+
+	//get list of map values and find max
+	mapValues := []int{}
+	for key := range arrMap {
+		mapValues = append(mapValues,arrMap[key])
+	}		
+	mode := -10000
+	for _, val := range mapValues {
+		if val > mode {
+			mode = val
+		}
+	}
+
+	//return final result, 1 or 0
+	if int(mean) == mode {
+		return 1
+	}
+	return 0
+
+}
+
+func main() {
+
+	fmt.Println(MeanMode([]int{5,3,3,3,1}))
+}
