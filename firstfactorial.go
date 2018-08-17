e0990e7c3a3110868d36a7d56f4e230fb1d03e4d
John Erickson <john.c.erickson@gmail.com>
2018-08-16 20:20:02 -0400

    put problem statement in

--- firstfactorial.go
+++ firstfactorial.go
@@ -1,5 +1,12 @@
-package main
+/*
+Have the function FirstFactorial(num) take the num 
+parameter being passed and return the factorial of it 
+(e.g. if num = 4, return (4 * 3 * 2 * 1)). For the test cases, 
+the range will be between 1 and 18 and the input will always 
+be an integer. 
+*/
 
+package main
 
 import (
 	"fmt"
@@ -15,9 +22,7 @@ func FirstFactorial(num int) (result int) {
 	return result
 }
 
-
-// this is a comment
 func main() {
-    fmt.Println(FirstFactorial(5))
-    fmt.Println(FirstFactorial(6))
+    fmt.Println(FirstFactorial(5)) //120
+    fmt.Println(FirstFactorial(6)) //720
 }
