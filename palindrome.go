d7c94b6ba6a4633b2c7bb751bbf85a9d6346f528
John Erickson <john.c.erickson@gmail.com>
2018-08-22 06:04:37 -0400

    optimized memory

--- palindrome.go
+++ palindrome.go
@@ -9,13 +9,13 @@ import (
 //ignores non-letters and letter case
 func IsPalindrome(s string) string {	
 
-	letters := []string{} //get only lowercase letters
-
-	for _, char := range s {
-		if unicode.IsLetter(rune(char)){
-			letters = append(letters, string(unicode.ToLower(char)))
+	//much faster,less memory allocations than loop below
+	letters := make([]rune, 0, len(s))
+	for _, char := range s{
+		if unicode.IsLetter(char){
+			letters = append(letters, unicode.ToLower(char))
 		}
-	}
+	}	
 	for i := 0; i < len(letters)/2; i++ {
 		if letters[i] != letters[len(letters)-i-1] {
 			return "false"
@@ -29,6 +29,12 @@ func main() {
 	fmt.Println(IsPalindrome("racecaR"))//true
 	fmt.Println(IsPalindrome("foxof")) //true
 	fmt.Println(IsPalindrome("racecer")) //false
-
 }
 
+/* slower than lines 13-18, allocates more memory
+letters := []string{} //get only lowercase letters
+for _, char := range s {
+	if unicode.IsLetter(rune(char)){
+		letters = append(letters, string(unicode.ToLower(char)))
+	}
+}*/
