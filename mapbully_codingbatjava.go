1e1f8858d7f261d11913f72768f5cf162ad68320
John Erickson <john.c.erickson@gmail.com>
2018-08-24 14:38:28 -0400

    all cases complete

--- mapbully_codingbatjava.go
+++ mapbully_codingbatjava.go
@@ -30,28 +30,34 @@ func mapBully (mapExample map[string]string) (map[string]string){
 
 	} else { //order matters here
 
-		//if there is no b key
-		if val, ok := mapExample["b"]; !ok {
-			//fmt.Println("no b key",val)
-			val = newMap["a"]
-			newMap["b"] = val
-			newMap["a"] = ""
-		} 
-
-		//replace map[b] with map[a]'s value
-		if val, ok := mapExample["b"]; ok {  
-			val = newMap["a"]
-	 		newMap["b"] = val
-				 		
-	 	} 
-
-	 	//replace map[a] with ""
-	 	if val, ok := mapExample["a"]; ok {  
-			val = ""
-			newMap["a"] = val
-	 	} 
-	}
-	
+		if val, ok := mapExample["a"]; !ok { //if there is no a key but is b
+			if val2, ok := mapExample["b"]; ok {
+				val = ""
+				newMap["a"] = val
+				val2 = mapExample["b"]
+				newMap["b"] = val2
+			}
+			
+		} else if val, ok := mapExample["b"]; !ok {  //if there is no b key but is a
+			if val2, ok := mapExample["a"]; ok {
+				val2 = ""
+				val = mapExample["a"]
+				newMap["b"] = val
+				newMap["a"] = val2
+			}			
+		} else { // both a and b keys exist 
+			//replace map[b] with map[a]'s value
+			if val, ok := mapExample["b"]; ok {
+				val = newMap["a"]
+			 	newMap["b"] = val			 		
+		 	} 
+		 	//replace map[a] with ""
+		 	if val, ok := mapExample["a"]; ok {  
+				val = ""
+				newMap["a"] = val
+		 	} 
+		}
+	}	
 	return newMap
 }
 
@@ -60,30 +66,37 @@ func main(){
 	map1 := make(map[string]string)
 	map1["a"] = "candy"
 	map1["b"] = "dirt"
-	fmt.Println(mapBully(map1))
+	fmt.Println("original =", map1, "solution =", mapBully(map1))
 	
 	map2 := make(map[string]string)
 	map2["a"] = "candy"
-	fmt.Println(mapBully(map2))
+	fmt.Println("original =", map2, "solution =", mapBully(map2))
 
 	map3 := make(map[string]string)
 	map3["a"] = "candy"
 	map3["b"] = "carrot"
 	map3["c"] = "meh"
-	fmt.Println(mapBully(map3))
+	fmt.Println("original =", map3, "solution =", mapBully(map3))
 
 	map4 := make(map[string]string)
 	map4["b"] = "carrot"
-	fmt.Println(mapBully(map4))
+	fmt.Println("original =", map4, "solution =", mapBully(map4))
 
 	map5 := make(map[string]string)
 	map5["c"] = "meh"
-	fmt.Println(mapBully(map5))
+	fmt.Println("original =",map5, "solution =",mapBully(map5))
 
 	map6 := make(map[string]string)
 	map6["a"] = "sparkle"
 	map6["c"] = "meh"
-	fmt.Println(mapBully(map6))
+	fmt.Println("original =",map6, "solution =",mapBully(map6))
+
+	map7 := make(map[string]string)
+	map7["b"] = "yo"
+	map7["c"] = "diddy"
+	map7["e"] = "pow"
+	map7["f"] = "not"
+	fmt.Println("original =", map7, "solution =", mapBully(map7))
 
 	//fmt.Printf("%t",map1)
 	
