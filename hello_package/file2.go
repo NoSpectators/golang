25d24b9eb28642301a64a00bb6c3a0398a75a362
John Erickson <john.c.erickson@gmail.com>
2018-09-08 12:25:14 -0400

    first folder attempt

---
+++ hello_package/file2.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func file2Func() {
+
+	fmt.Println("this line is from file2.go --test file 2 works")
+}  
---
+++ hello_package/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, 世界") 
+	file2Func()
+}
+
+
